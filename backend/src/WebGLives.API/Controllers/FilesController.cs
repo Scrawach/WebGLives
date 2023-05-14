256fa711c81b3e8d44e451704b4fc4e9a68d2aa6
scrawach <scrawach@gmail.com>
2023-05-14 19:28:19 +0300

    added temps files and games controller

---
+++ backend/src/WebGLives.API/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using System.IO.Compression;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebGLives.API.Controllers;
+
+[ApiController]
+[Route("api/upload")]
+public class FilesController : ControllerBase
+{
+    private readonly ILogger<FilesController> _logger;
+    private static readonly string BaseDirectory = Path.Combine(Path.GetTempPath(), "games");
+
+    public FilesController(ILogger<FilesController> logger) => 
+        _logger = logger;
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+    public async Task<ActionResult> Post(IFormFile file)
+    {
+        if (DirectoryNotExist())
+            CreateDirectory();
+
+        var filePath = Path.Combine(BaseDirectory, file.FileName);
+        await using (var stream = new FileStream(filePath, FileMode.Create))
+            await file.CopyToAsync(stream);
+
+        ExtractFilesFrom(filePath);
+        return Ok();
+    }
+
+    private static void ExtractFilesFrom(string filePath) => 
+        ZipFile.ExtractToDirectory(filePath, Path.Combine(BaseDirectory, "unzip"));
+
+    private static bool DirectoryNotExist() => 
+        !Directory.Exists(BaseDirectory);
+
+    private static DirectoryInfo CreateDirectory() => 
+        Directory.CreateDirectory(BaseDirectory);
+}
---
+++ backend/src/WebGLives.API/Controllers/GamesController.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebGLives.API.Controllers;
+
+[ApiController]
+[Route("api/games")]
+public class GamesController : Controller
+{
+    private readonly ILogger<GamesController> _logger;
+    private static readonly string BaseDirectory = Path.Combine(Path.GetTempPath(), "games");
+
+    public GamesController(ILogger<GamesController> logger) => 
+        _logger = logger;
+
+    [HttpGet]
+    [Route("{gameName}")]
+    public IActionResult Index(string gameName)
+    {
+        if (DirectoryNotExist())
+            CreateDirectory();
+        
+        var pathToGame = Path.Combine(BaseDirectory, gameName);
+
+        if (Directory.Exists(pathToGame))
+            return GamePage(pathToGame);
+        
+        return HelloWorldPage();
+    }
+
+    private IActionResult GamePage(string pathToGame)
+    {
+        var filePath = Path.Combine(pathToGame, "index.html");
+        var fileBytes = System.IO.File.ReadAllBytes(filePath);
+        return File(fileBytes, "application/octet-stream");
+    }
+
+    private static ContentResult HelloWorldPage() =>
+        new ContentResult()
+        {
+            ContentType = "text/html",
+            StatusCode = (int)HttpStatusCode.OK,
+            Content = "<html><body>Hello World</body></html>"
+        };
+
+    private static bool DirectoryNotExist() => 
+        !Directory.Exists(BaseDirectory);
+
+    private static DirectoryInfo CreateDirectory() => 
+        Directory.CreateDirectory(BaseDirectory);
+}
