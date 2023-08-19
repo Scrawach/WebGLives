24dd973864b7637420b37dcf5ee6587024e7ae58
scrawach <scrawach@gmail.com>
2023-08-19 21:18:23 +0300

    added useEffect hook for render dashboard game cards in first time

--- frontend/WebGLives/src/pages/Dashboard.tsx
+++ frontend/WebGLives/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Grid } from "@chakra-ui/react";
 import { GameCardData } from "../types/GameCardData";
 import { GameCard } from "../components/GameCard"
@@ -6,14 +6,17 @@ import { Api } from "../services/Api"
 
 export const Dashboard: React.FC = () => {
     const [gamePages, setGamePages] = useState<GameCardData[]>([]);
-    const getGamePages = async () => {
-        var pages = await Api.gamePages();
-        setGamePages(pages);
-    }
+
+    useEffect(() => {
+        async function fetchGamePages() {
+            var pages = await Api.gamePages();
+            setGamePages(pages);
+        }
+        fetchGamePages();
+    }, [])
 
     return (
         <>
-            <Button onClick={getGamePages}>Load Games</Button>
             <Grid templateColumns='repeat(5, 1fr)' gap={6}>
                 {gamePages.map(game => (
                     <GameCard
