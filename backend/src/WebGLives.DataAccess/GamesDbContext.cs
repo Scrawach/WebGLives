a7ef2fea968850745a7209f2de1f69dd1e5f55af
scrawach <scrawach@gmail.com>
2023-09-06 13:45:23 +0300

    apply game entity configuration at db context

--- backend/src/WebGLives.DataAccess/GamesDbContext.cs
+++ backend/src/WebGLives.DataAccess/GamesDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using WebGLives.DataAccess.Configurations;
 using WebGLives.DataAccess.Entities;
 
 namespace WebGLives.DataAccess;
@@ -8,4 +9,9 @@ public class GamesDbContext : DbContext
     public GamesDbContext(DbContextOptions<GamesDbContext> options) : base(options) { }
     
     public DbSet<GameEntity> Games { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new GameEntityConfiguration());
+    }
 }
