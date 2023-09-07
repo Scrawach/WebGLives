3ed441e5c5cfd35623e1fb550e99289a44f8843d
scrawach <scrawach@gmail.com>
2023-09-07 15:02:42 +0300

    update navigation bar

--- frontend/WebGLives/src/components/NavigationBar.tsx
+++ frontend/WebGLives/src/components/NavigationBar.tsx
@@ -1,19 +1,40 @@
-import { Box, Spacer, Link, HStack, useColorModeValue } from "@chakra-ui/react";
+import { Box, Spacer, Link, HStack, useColorModeValue, Button, Input, InputLeftElement, LinkOverlay, InputGroup } from "@chakra-ui/react";
+import { SmallAddIcon, SearchIcon, CalendarIcon } from "@chakra-ui/icons";
 import { ColorModeSwitcher } from "../ColorModeSwitcher";
 
 export const NavigationBar : React.FC = () => {
+    const bgColor = useColorModeValue("gray.300", "gray.700");
+
     return (
-        <Box
-          bg = {useColorModeValue('gray.300', 'gray.700')}
-        >
+        <Box>
           <HStack
             p={4}
             spacing={8}
             align="center"
           >
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/upload">Upload</Link>
+            
+            <Button variant="ghost" leftIcon={<CalendarIcon />}>
+              <LinkOverlay href="/">
+                Dashboard
+              </LinkOverlay>
+            </Button>
+
             <Spacer />
+            
+            <InputGroup width = "50%">
+              <InputLeftElement pointerEvents="none">
+                <SearchIcon />
+              </InputLeftElement>
+              <Input type="tel" placeholder="Search..." />
+            </InputGroup>
+
+            
+            <Button leftIcon={<SmallAddIcon />}>
+              <LinkOverlay href="/create"> 
+                New Game
+              </LinkOverlay>
+            </Button>
+ 
             <ColorModeSwitcher />
           </HStack>
         </Box>
