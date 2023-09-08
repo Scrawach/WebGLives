505125a99b0a1251c949fd494929f0294bf95c0f
scrawach <scrawach@gmail.com>
2023-09-08 09:35:20 +0300

    install json-server dependency

--- frontend/WebGLives/src/App.tsx
+++ frontend/WebGLives/src/App.tsx
@@ -9,12 +9,14 @@ import {
 import { NavigationBar } from "./components/NavigationBar";
 import UploadPage from "./pages/UploadPage";
 import { CreateGame, createAction } from "./pages/CreateGame";
+import { GamePage } from "./pages/GamePage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route index element={<Dashboard />} />
       <Route path="create" element={<CreateGame />} action={createAction} />
+      <Route path="game/:id" element={<GamePage />} />
     </Route>
   )
 )
