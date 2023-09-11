9e37079a45b9d173e3e5ba8c86db035261b31e5d
scrawach <scrawach@gmail.com>
2023-09-11 11:53:31 +0300

    a little bit refactoring for games contoller, added async response mapping for results

--- backend/src/WebGLives.API/Controllers/FunctionalControllerBase.cs
+++ backend/src/WebGLives.API/Controllers/FunctionalControllerBase.cs
@@ -6,17 +6,33 @@ namespace WebGLives.API.Controllers;
 
 public abstract class FunctionalControllerBase : ControllerBase
 {
-    public IActionResult ResponseFrom<TResult, TError>(Result<TResult, TError> result) where TError : Error =>
+    protected async Task<IActionResult> AsyncResponseFrom<TResult, TError>(Task<Result<TResult, TError>> resultTask) where TError : Error
+    {
+        var result = await resultTask.ConfigureAwait(Result.Configuration.DefaultConfigureAwait);
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : ResponseFrom(result.Error);
+    }
+    
+    protected async Task<IActionResult> AsyncResponseFrom<TError>(Task<UnitResult<TError>> resultTask) where TError : Error
+    {
+        var unitResult = await resultTask.ConfigureAwait(Result.Configuration.DefaultConfigureAwait);
+        return unitResult.IsSuccess
+            ? Ok()
+            : ResponseFrom(unitResult.Error);
+    }
+
+    protected IActionResult ResponseFrom<TResult, TError>(Result<TResult, TError> result) where TError : Error =>
         result.IsSuccess
             ? Ok(result.Value)
             : ResponseFrom(result.Error);
 
-    public IActionResult ResponseFrom<TError>(UnitResult<TError> result) where TError : Error =>
+    protected IActionResult ResponseFrom<TError>(UnitResult<TError> result) where TError : Error =>
         result.IsSuccess
             ? Ok()
             : ResponseFrom(result.Error);
 
-    public IActionResult ResponseFrom<TError>(TError error) where TError : Error =>
+    private IActionResult ResponseFrom<TError>(TError error) where TError : Error =>
         error switch
         {
             NotFoundError notFound => NotFound(notFound.Message),
--- backend/src/WebGLives.API/Controllers/GamesController.cs
+++ backend/src/WebGLives.API/Controllers/GamesController.cs
@@ -17,78 +17,54 @@ public class GamesController : FunctionalControllerBase
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Game>))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-    public async Task<IActionResult> All()
-    {
-        var result = await _games.All();
-        return ResponseFrom(result);
-    }
+    public async Task<IActionResult> All() =>
+        await AsyncResponseFrom(_games.All());
 
     [HttpGet("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Game))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-    public async Task<IActionResult> Get(int id)
-    {
-        var result = await _games.Get(id);
-        return ResponseFrom(result);
-    }
-    
+    public async Task<IActionResult> Get(int id) =>
+        await AsyncResponseFrom(_games.Get(id));
+
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-    public async Task<IActionResult> Create()
-    {
-        var createdResult = await _games.Create();
-        return ResponseFrom(createdResult);
-    }
+    public async Task<IActionResult> Create() =>
+        await AsyncResponseFrom(_games.Create());
 
     [HttpPut("{id:int}/title")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-    public async Task<IActionResult> UpdateTitle(int id, string title)
-    {
-        var result = await _games.UpdateTitle(id, title);
-        return ResponseFrom(result);
-    }
-    
+    public async Task<IActionResult> UpdateTitle(int id, string title) =>
+        await AsyncResponseFrom(_games.UpdateTitle(id, title));
+
     [HttpPut("{id:int}/description")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-    public async Task<IActionResult> UpdateDescription(int id, string description)
-    {
-        var result = await _games.UpdateDescription(id, description);
-        return ResponseFrom(result);
-    }
-    
+    public async Task<IActionResult> UpdateDescription(int id, string description) =>
+        await AsyncResponseFrom(_games.UpdateDescription(id, description));
+
     [HttpPut("{id:int}/poster")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-    public async Task<IActionResult> UpdatePoster(int id, IFormFile poster)
-    {
-        var result = await _games.UpdatePoster(id, poster.OpenReadStream());
-        return ResponseFrom(result);
-    }
-    
+    public async Task<IActionResult> UpdatePoster(int id, IFormFile poster) =>
+        await AsyncResponseFrom(_games.UpdatePoster(id, poster.OpenReadStream()));
+
     [HttpPut("{id:int}/game")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-    public async Task<IActionResult> UpdateGame(int id, IFormFile archive)
-    {
-        var result = await _games.UpdateGame(id, archive.OpenReadStream());
-        return ResponseFrom(result);
-    }
+    public async Task<IActionResult> UpdateGame(int id, IFormFile archive) =>
+        await AsyncResponseFrom(_games.UpdateGame(id, archive.OpenReadStream()));
 
     [HttpDelete("{id:int}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-    public async Task<IActionResult> Delete(int id)
-    {
-        var result = await _games.Delete(id);
-        return ResponseFrom(result);
-    }
+    public async Task<IActionResult> Delete(int id) =>
+        await AsyncResponseFrom(_games.Delete(id));
 }
