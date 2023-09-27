06d1789a5789fcb91ef02adceb3c1351449a9f81
scrawach <scrawach@gmail.com>
2023-09-27 16:40:00 +0300

    change lifetime for token service to scoped

--- backend/src/WebGLives.API/Extensions/ServiceCollectionExtensions.cs
+++ backend/src/WebGLives.API/Extensions/ServiceCollectionExtensions.cs
@@ -42,7 +42,7 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IZipService, ZipService>();
         services.AddSingleton<IFilesService, FilesService>();
         services.AddSingleton<IJwtTokenService, JwtTokenService>();
-        services.AddSingleton<ITokenFactory, TokenFactory>();
+        services.AddScoped<ITokenFactory, TokenFactory>();
         services.AddScoped<IGamesService, GamesService>();
         services.AddScoped<IGamesRepository, GamesRepository>();
 
