9a1562dbd4bd91b3b0026f3eddb625e6d25992c1
scrawach <scrawach@gmail.com>
2023-09-28 17:19:13 +0300

    removed unused methods from functional controller base

--- backend/src/WebGLives.API/Controllers/FunctionalControllerBase.cs
+++ backend/src/WebGLives.API/Controllers/FunctionalControllerBase.cs
@@ -6,28 +6,17 @@ namespace WebGLives.API.Controllers;
 
 public abstract class FunctionalControllerBase : ControllerBase
 {
-    protected async Task<IActionResult> AsyncResponseFrom<TResult, TError>(Task<Result<TResult, TError>> resultTask) where TError : Error
-    {
-        var result = await resultTask.ConfigureAwait(Result.Configuration.DefaultConfigureAwait);
-        return ResponseFrom(result);
-    }
-    
-    protected async Task<IActionResult> AsyncResponseFrom<TError>(Task<UnitResult<TError>> resultTask) where TError : Error
-    {
-        var unitResult = await resultTask.ConfigureAwait(Result.Configuration.DefaultConfigureAwait);
-        return ResponseFrom(unitResult);
-    }
+    protected async Task<IActionResult> AsyncResponseFrom<TResult, TError>(Task<Result<TResult, TError>> resultTask) where TError : Error =>
+        ResponseFrom(await resultTask.ConfigureAwait(Result.Configuration.DefaultConfigureAwait));
 
+    protected async Task<IActionResult> AsyncResponseFrom<TError>(Task<UnitResult<TError>> resultTask) where TError : Error =>
+        ResponseFrom(await resultTask.ConfigureAwait(Result.Configuration.DefaultConfigureAwait));
+    
     protected IActionResult ResponseFrom<TResult, TError>(Result<TResult, TError> result) where TError : Error =>
         result.IsSuccess
             ? Ok(result.Value)
             : ResponseFrom(result.Error);
     
-    protected IActionResult ResponseFrom<TResult>(Result<TResult> result) =>
-        result.IsSuccess
-            ? Ok(result.Value)
-            : BadRequest(result.Error);
-
     protected IActionResult ResponseFrom<TError>(UnitResult<TError> result) where TError : Error =>
         result.IsSuccess
             ? Ok()
