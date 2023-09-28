9ef50ac007a625947c2c41351a86695d59fa403c
scrawach <scrawach@gmail.com>
2023-09-28 17:18:05 +0300

    a little bit refactoring for functional controller base

--- backend/src/WebGLives.API/Controllers/FunctionalControllerBase.cs
+++ backend/src/WebGLives.API/Controllers/FunctionalControllerBase.cs
@@ -9,17 +9,13 @@ public abstract class FunctionalControllerBase : ControllerBase
     protected async Task<IActionResult> AsyncResponseFrom<TResult, TError>(Task<Result<TResult, TError>> resultTask) where TError : Error
     {
         var result = await resultTask.ConfigureAwait(Result.Configuration.DefaultConfigureAwait);
-        return result.IsSuccess
-            ? Ok(result.Value)
-            : ResponseFrom(result.Error);
+        return ResponseFrom(result);
     }
     
     protected async Task<IActionResult> AsyncResponseFrom<TError>(Task<UnitResult<TError>> resultTask) where TError : Error
     {
         var unitResult = await resultTask.ConfigureAwait(Result.Configuration.DefaultConfigureAwait);
-        return unitResult.IsSuccess
-            ? Ok()
-            : ResponseFrom(unitResult.Error);
+        return ResponseFrom(unitResult);
     }
 
     protected IActionResult ResponseFrom<TResult, TError>(Result<TResult, TError> result) where TError : Error =>
