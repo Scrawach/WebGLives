cd374c4dab16719e6eb0fd9319821dfca7646d7c
scrawach <scrawach@gmail.com>
2023-09-29 14:17:09 +0300

    added token service to API static class

--- frontend/WebGLives/src/services/Api.ts
+++ frontend/WebGLives/src/services/Api.ts
@@ -1,8 +1,10 @@
 import { GameService } from "./GameService";
+import { TokenService } from "./TokenService";
 
 export class Api {
     private static readonly url: string = "http://localhost:5072"
     private static readonly jsonServerUrl: string = "http://localhost:3001"
 
-    public static readonly games: GameService = new GameService(Api.jsonServerUrl);
+    public static readonly games: GameService = new GameService(Api.url);
+    public static readonly tokens: TokenService = new TokenService(Api.url);
 }
