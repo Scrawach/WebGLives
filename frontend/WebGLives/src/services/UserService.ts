e39e8f1f0d791815772b5167e15da1db823fe9d2
scrawach <scrawach@gmail.com>
2023-10-06 16:25:33 +0300

    implemented introduce to user service

---
+++ frontend/WebGLives/src/services/UserService.ts
@@ -0,0 +1,9 @@
+export class UserService {
+    public static register(login: string, password: string): void {
+
+    }
+
+    public static login(login: string, password: string): void {
+
+    }
+}
