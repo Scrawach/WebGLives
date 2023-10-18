aed84f3a52d6fea90828e70d9e686ee0b89618e1
scrawach <scrawach@gmail.com>
2023-10-18 10:24:08 +0300

    moved initialize tests code inside base test controller

---
+++ backend/src/WebGLives.Tests.Integration/Controllers/UsersControllerTests.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using FluentAssertions;
+
+namespace WebGLives.Tests.Integration.Controllers;
+
+public class UsersControllerTests : ControllerTestsBase
+{
+    [Theory]
+    [InlineData("test", "test123")]
+    public async Task WhenCreateUser_ThenShouldReturnOkStatus(string login, string password)
+    {
+        var response = await Post(CreateUserRequest(login, password));
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Theory]
+    [InlineData("test", "")]
+    [InlineData("test", "test")]
+    [InlineData("test", "test1")]
+    public async Task WhenCreateUser_AndPasswordLessThan6Symbols_ThenShouldReturnBadRequest(string login, string password)
+    {
+        var response = await Post(CreateUserRequest(login, password));
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Theory]
+    [InlineData("", "test123")]
+    public async Task WhenCreateUser_AndEmptyLogin_ThenShouldReturnBadRequest(string login, string password)
+    {
+        var response = await Post(CreateUserRequest(login, password));
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    private Task<HttpResponseMessage> Post(HttpContent content) =>
+        Client.PostAsync("users", content);
+
+    private static FormUrlEncodedContent CreateUserRequest(string login, string password) =>
+        new(new[]
+        {
+            new KeyValuePair<string, string>("Login", login),
+            new KeyValuePair<string, string>("Password", password)
+        });
+}
