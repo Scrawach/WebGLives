4d733474fb9e9dd23a36e441ae8beae7c998a3b6
scrawach <scrawach@gmail.com>
2023-10-20 18:09:18 +0300

    tests: refactoring for games service, separated method for game with access check

--- backend/src/WebGLives.BusinessLogic/Services/GamesService.cs
+++ backend/src/WebGLives.BusinessLogic/Services/GamesService.cs
@@ -32,8 +32,7 @@ public class GamesService : IGamesService, IGamesChangeService
         await _games.GetOrDefault(gameId, token);
 
     public async Task<UnitResult<Error>> Update(int userId, int gameId, UpdateGameData data, CancellationToken token = default) =>
-        await _games.GetOrDefault(gameId, token)
-            .Ensure(game => game.UserId == userId, new InvalidAccessRightToGame(userId, gameId))
+        await GameWithAccessCheck(userId, gameId, token)
             .TapIf(data.Title is not null, game => game.Title = data.Title)
             .TapIf(data.Description is not null, game => game.Description = data.Description)
             .CheckIf(data.Poster is not null, async game => await _files
@@ -45,37 +44,35 @@ public class GamesService : IGamesService, IGamesChangeService
             .Check(game => _games.Update(game, token));
 
     public async Task<UnitResult<Error>> Delete(int userId, int gameId, CancellationToken token = default) =>
-        await _games.GetOrDefault(gameId, token)
-            .Ensure(game => true, new InvalidAccessRightToGame(userId, gameId))
+        await GameWithAccessCheck(userId, gameId, token)
             .Check(_ => _files.Delete(gameId.ToString()))
             .Check(_ => _games.Delete(gameId, token));
 
     public async Task<UnitResult<Error>> UpdateTitle(int userId, int gameId, string title, CancellationToken token = default) =>
-        await _games.GetOrDefault(gameId, token)
-            .Ensure(game => game.UserId == userId, new InvalidAccessRightToGame(userId, gameId))
+        await GameWithAccessCheck(userId, gameId, token)
             .Tap(game => game.Title = title)
             .Check(game => _games.Update(game, token));
 
     public async Task<UnitResult<Error>> UpdateDescription(int userId, int gameId, string description, CancellationToken token = default) =>
-        await _games.GetOrDefault(gameId, token)
-            .Ensure(game => game.UserId == userId, new InvalidAccessRightToGame(userId, gameId))
+        await GameWithAccessCheck(userId, gameId, token)
             .Tap(game => game.Description = description)
             .Check(game => _games.Update(game, token));
 
     public async Task<UnitResult<Error>> UpdateGame(int userId, int gameId, FileData file, CancellationToken token = default) =>
-        await _games.GetOrDefault(gameId, token)
-            .Ensure(game => game.UserId == userId, new InvalidAccessRightToGame(userId, gameId))
+        await GameWithAccessCheck(userId, gameId, token)
             .Check(async game => await _files
                 .SaveZip(gameId.ToString(), file, token)
                 .Tap(path => game.GameUrl = path))
             .Check(game => _games.Update(game, token));
 
     public async Task<UnitResult<Error>> UpdatePoster(int userId, int gameId, FileData file, CancellationToken token = default) =>
-        await _games.GetOrDefault(gameId, token)
-            .Ensure(game => game.UserId == userId, new InvalidAccessRightToGame(userId, gameId))
+        await GameWithAccessCheck(userId, gameId, token)
             .Check(async game => await _files
                 .Save(gameId.ToString(), file, token)
                 .Tap(path => game.PosterUrl = path))
             .Check(game => _games.Update(game, token));
-    
+
+    private Task<Result<Game, Error>> GameWithAccessCheck(int userId, int gameId, CancellationToken token) =>
+        _games.GetOrDefault(gameId, token)
+            .Ensure(game => game.UserId == userId, new InvalidAccessRightToGame(userId, gameId));
 }
