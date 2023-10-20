b6e722a52af99f116fcf82605a0b8d2f7e6ff32f
scrawach <scrawach@gmail.com>
2023-10-20 19:31:36 +0300

    api: a little bit name refactoring for games catalog controller

--- backend/src/WebGLives.API/Controllers/GamesCatalogController.cs
+++ backend/src/WebGLives.API/Controllers/GamesCatalogController.cs
@@ -8,21 +8,21 @@ namespace WebGLives.API.Controllers;
 [Route("Games")]
 public class GamesCatalogController : FunctionalControllerBase
 {
-    private readonly IGamesCatalogService _gamesCatalog;
+    private readonly IGamesCatalogService _games;
 
-    public GamesCatalogController(IGamesCatalogService gamesCatalog) =>
-        _gamesCatalog = gamesCatalog;
+    public GamesCatalogController(IGamesCatalogService games) =>
+        _games = games;
     
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<GameResponse>))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
     public async Task<IActionResult> All(CancellationToken token = default) =>
-        await ResponseFromAsync(GameResponse.From(_gamesCatalog.All(token)));
+        await ResponseFromAsync(GameResponse.From(_games.All(token)));
 
     [HttpGet("{gameId:int}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GameResponse))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
     public async Task<IActionResult> Get(int gameId, CancellationToken token = default) =>
-        await ResponseFromAsync(GameResponse.From(_gamesCatalog.Get(gameId, token)));
+        await ResponseFromAsync(GameResponse.From(_games.Get(gameId, token)));
 }
