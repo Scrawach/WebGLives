1f9da8c11a2022239d76fbe0cdb40f095a4e3c74
scrawach <scrawach@gmail.com>
2023-10-30 17:04:38 +0300

    api: implemented policy setting for allow concrete methods or headers

--- backend/WebGLives/WebGLives.API/Options/CorsPolicySetting.cs
+++ backend/WebGLives/WebGLives.API/Options/CorsPolicySetting.cs
@@ -7,16 +7,16 @@ namespace WebGLives.API.Options;
 public class CorsPolicySetting
 {
     public string[]? Origins { get; set; }
-    public bool? AllowAnyMethods { get; set; }
-    public bool? AllowAnyHeaders { get; set; }
+    public string[]? AllowHeaders { get; set; }
+    public string[]? AllowMethods { get; set; }
     public bool? AllowCredentials { get; set; }
 
     public void Configure(CorsPolicyBuilder policy) =>
         policy
             .ToResult(new Error("To Result Cast Error"))
             .TapIf(Origins is not null, () => policy.WithOrigins(Origins!))
-            .TapIf(AllowAnyMethods is not null, () => policy.AllowAnyMethod())
-            .TapIf(AllowAnyHeaders is not null, () => policy.AllowAnyHeader())
+            .TapIf(AllowHeaders is not null, () => policy.WithHeaders(AllowHeaders!))
+            .TapIf(AllowMethods is not null, () => policy.WithMethods(AllowMethods!))
             .TapIf(AllowCredentials is not null, () => policy.AllowCredentials());
 
     public static implicit operator Action<CorsPolicyBuilder>(CorsPolicySetting setting) =>
